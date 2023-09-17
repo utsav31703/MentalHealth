0c8c11da70422991e6db56b83158728a74194f07
Utsav kushwaha <94130476+utsav31703@users.noreply.github.com>
2023-09-17 21:43:28 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,129 @@
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+
+startBtn.onclick = () => {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
+};
+
+exitBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizBox.classList.add("active");
+
+  showQuestions(0);
+  questionCounter(1);
+  headerScore();
+};
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestions(questionCount);
+
+    questionNumb++;
+    questionCounter(questionNumb);
+
+    nextBtn.classList.remove("active");
+  } else {
+    showResultBox();
+  }
+};
+
+const optionList = document.querySelector(".option-list");
+
+function showQuestions(index) {
+  const questionText = document.querySelector(".question-text");
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = ` <div class="option"><span>${questions[index].options[0]}</span></div>
+  <div class="option"><span>${questions[index].options[1]}</span></div>
+  <div class="option"><span>${questions[index].options[2]}</span></div>
+  <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+  optionList.innerHTML = optionTag;
+
+  const option = document.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+  let allOptions = optionList.children.length;
+
+  if (userAnswer == correctAnswer) {
+    answer.classList.add("correct");
+    userScore += 1;
+    headerScore();
+  } else {
+    answer.classList.add("incorrect");
+
+    //if answer is incorect ,auto select the correct answer
+    for (let i = 0; i < allOptions; i++) {
+      if (optionList.children[i].textContent == correctAnswer) {
+        optionList.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+
+  //if user has selcted disabled all options
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add("disabled");
+  }
+
+  nextBtn.classList.add("active");
+}
+
+function questionCounter(index) {
+  const questionTotal = document.querySelector(".question-total");
+  questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore() {
+  const headerScoreText = document.querySelector(".header-score");
+  headerScoreText.textContent = `score: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+  quizBox.classList.remove("active");
+  resultBox.classList.add("active");
+
+  const ScoreText = document.querySelector(".score-text");
+  ScoreText.textContent = `Your Score${userScore} out of ${questions.length}`;
+
+  const circularProgress = document.querySelector(".circular-progress");
+  const ProgressValue = document.querySelector(".progress-value");
+  let ProgressStartValue = -1;
+  let ProgressEndValue = (userScore / questions.length) * 100;
+  let speed = 20;
+
+  let progress = setInterval(() => {
+    ProgressStartValue++;
+    circularProgress.style.background=`conic-gradient(#c40094 ${ProgressStartValue * 3.6}deg, rgba(255, 255, 255, 0.1) 0deg)`
+    ProgressValue.textContent = `${ProgressStartValue}%`;
+    if (ProgressStartValue == ProgressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+}
---
+++ que.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Over the past two weeks, how often have you felt little interest or pleasure in doing things you usually enjoy?",
+    answer: " C. Not at all",
+    options: [
+      " A. More than half the days",
+      " B. Several days",
+      " C. Not at all",
+      " D. Nearly every day",
+    ],
+  },
+
+  {
+    numb: 2,
+    question:
+      "In the last month, how often have you had trouble falling asleep or staying asleep, or have you been sleeping too much? ",
+    answer: "D. Not at all ",
+    options: [
+      "A. Nearly Every Day",
+      "B. Several days",
+      "C. More than half the days",
+      "D. Not at all ",
+    ],
+  },
+  // {
+  //   numb: 3,
+  //   question:
+  //   "Have you experienced a significant change in your appetite or weight (either loss or gain) in the past month?",
+
+  //   answer: "D. Not at all ",
+  //   options: [
+  //     "A. Nearly Every Day",
+  //     "B. Several days",
+  //     "C. More than half the days",
+  //     "D. Not at all ",
+  //   ],
+  // },
+
+  {
+    numb: 3,
+    question:
+      "Have you experienced a significant change in your appetite or weight (either loss or gain) in the past month?",
+
+    answer:" D. No",
+    options: [
+      " A. Yes, a slight change",
+      " B. Yes, a moderate change",
+      " C. Yes, a significant change",
+      " D. No",
+    ],
+  },
+
+  {
+    numb: 4,
+    question:
+      "How often have you felt unusually tired or lacking in energy over the past two weeks? ",
+
+    answer: "C. NO",
+    options: [
+      "A. More than half the days",
+      "B. Several days",
+      "C. NO",
+      "D. Nearly every day",
+    ],
+  },
+
+  {
+    numb: 5,
+    question:
+      "In the last month, have you had thoughts of harming yourself or ending your life? ",
+
+    answer: " D. No",
+    options: [
+      " A. Yes, and I've attempted self-harm or suicide",
+      " B. Yes, but they were fleeting and I didn't have a plan",
+      " C. Yes, and I've had a plan but haven't attempted",
+      " D. No",
+    ],
+  },
+];
